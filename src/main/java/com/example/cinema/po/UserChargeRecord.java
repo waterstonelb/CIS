8f481e79cfcdf7f954ceb00415985f04f609572f
uncln <37567935+uncln@users.noreply.github.com>
2019-06-08 11:05:31 +0800

    1222333

---
+++ src/main/java/com/example/cinema/po/UserChargeRecord.java
@@ -0,0 +1,27 @@
+package com.example.cinema.po;
+
+/**
+ * @author yzh
+ * @date 2019/6/7 ???
+ */
+public class UserChargeRecord {
+    private Double chargeNum;
+    private String time;
+
+    public Double getchargeNum() {
+        return chargeNum;
+    }
+
+    public void setchargeNum(Double chargeNum) {
+        this.chargeNum = chargeNum;
+    }
+
+    public String gettime() {
+        return time;
+    }
+
+    public void settime(String time) {
+        this.time = time;
+    }
+
+}
