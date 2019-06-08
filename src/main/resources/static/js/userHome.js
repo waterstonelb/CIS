4da54834e35808844428bd75d6c0617dc005a161
myc <ytmbx@126.com>
2019-06-08 11:43:18 +0800

    edit the homepage

--- src/main/resources/static/js/userHome.js
+++ src/main/resources/static/js/userHome.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     var movieList;
     getMovies();
+    getTopBox();
+   // getMostFavourite();
     function getMovies() {
         getRequest(
             "/movie/all/exclude/off",
@@ -13,6 +15,24 @@ $(document).ready(function() {
             }
         )
     }
+    function getTopBox() {
+        getRequest(
+            "/statistics/boxOffice/total",
+            function (res) {
+                topList=res.content;
+                renderTopBox(topList)
+            }
+        )
+    }
+    function getMostFavourite(){
+        getRequest(
+            "/statistics/mostFavourite",
+            function (res) {
+                favouriteList=res.content;
+                renderMostFavourite(favouriteList)
+            }
+        )
+    }
     function renderMovie(movieList) {
         $('.movie-on-list').empty();
         var movieDomStr = '';
@@ -20,11 +40,42 @@ $(document).ready(function() {
             movie.description = movie.description || '';
             movieDomStr +=
                 "<li class='movie-item card'>" +
-                "<img class='movie-img' src='" + (movie.posterUrl || "../images/defaultAvatar.jpg") + "'/>" +
+                "<img class='movie-img' src='" + (movie.posterUrl || "../images/defaultAvatar.jpg") +"' id='"+movie.id + "'/>" +
                 "<div>"+movie.name+"</div>"+
                 "</li>";
         });
         $('.movie-on-list').append(movieDomStr);
     }
+    function renderTopBox(topList) {
+        $('.top-sell-list').empty();
+        var movieDomStr = '';
+        for(i=0;i<topList.length&&10;i++){
+            movieDomStr +=
+                "<div class='statistic-item' id='"+topList[i].movieId+"'>" +
+                "<span>"+topList[i].name+"</span>"+
+                "<span>"+(topList[i].boxOffice || 0)+"</span>"+
+                "</div>";
+        }
+        $('.top-sell-list').append(movieDomStr);
+    }
+    function renderMostFavourite(favouriteList) {
+        $('.top-expectation-list').empty();
+        var movieDomStr = '';
+        for(i=0;i<favouriteList.length&&10;i++){
+            movieDomStr +=
+                "<div class='statistic-item'>" +
+                "<span>"+topList[i].moviename+"</span>"+
+                "<span>"+(favouriteList[i].likeNum || 0)+"</span>"+
+                "</div>";
+        }
+        $('.top-expectation-list').append(movieDomStr);
+    }
+
+    $(document).on('click','.movie-img',function(){
+        $(location).attr('href', '/user/movieDetail?id='+$(this).attr("id"));
+    });
+    $(document).on('click','.statistic-item',function(){
+        $(location).attr('href', '/user/movieDetail?id='+$(this).attr("id"));
+    });
     
 });
